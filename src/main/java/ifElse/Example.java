bebeff8470006b8d2dd73ea67175f5cb4d74c416
Diana <abdyrahunovadiana@gmail.com>
2024-05-13 20:19:11 +0600

    some changes

--- src/main/java/ifElse/Example.java
+++ src/main/java/ifElse/Example.java
@@ -2,7 +2,7 @@ package ifElse;
 
 public class Example {
     public static void main(String[] args) {
-    //diana
+    //diana jfjjhgfjhgkjh
         int number = 10;
         if (number % 2 == 0) {
             System.out.println(number + " является четным числом.");
